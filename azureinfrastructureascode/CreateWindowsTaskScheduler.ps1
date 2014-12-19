140922fa71cef3142ec8e89b5b3452eca7503b55
unknown <mani@dinventive.com>
2014-12-19 16:00:40 +0000

    Scripts to Backup files to the azure fileshares

---
+++ azureinfrastructureascode/CreateWindowsTaskScheduler.ps1
@@ -0,0 +1,45 @@
+﻿<#
+    .SYNOPSIS
+        Template script
+    .DESCRIPTION
+        This script sets up the the Task scheduler for backup.
+    .PARAMETER
+    .EXAMPLE
+    .NOTES
+        ScriptName : CreateWindowsTaskScheduler.ps1
+        Created By : Manimaran Chandrasekaran
+        Date Coded : 12/19/2014 07:51:20
+
+    .LINK
+        https://www.devopspractice.com/
+#>
+
+#Define all the variables
+
+$TName = "BackupDatabase"
+$TDesc = "Run a Backup powershell script through a scheduled task"
+$TCmd = "c:\windows\system32\WindowsPowerShell\v1.0\powershell.exe"
+$TScript = "C:\Users\mani_000\Documents\AzureAutomation\azureinfrastructureascode\Schedulebackup.ps1"
+$TArg = "-WindowStyle Hidden -NonInteractive -Executionpolicy unrestricted -file $TScript"
+$TaskStartTime = [datetime]::Now.AddHours(1)
+
+$svc = new-object -ComObject("Schedule.Service")
+$svc.Connect()
+$Folder = $svc.GetFolder("\")
+
+$TaskDef = $svc.NewTask(0) 
+$TaskDef.RegistrationInfo.Description = "$TDesc"
+$TaskDef.Settings.Enabled = $true
+$TaskDef.Settings.AllowDemandStart = $true
+#Define triggers just one time in this case
+$trgs = $TaskDef.Triggers
+$trigger = $trgs.Create(1) 
+$trigger.StartBoundary = $TaskStartTime.ToString("yyyy-MM-dd'T'HH:mm:ss")
+$trigger.Enabled = $true
+
+$Action = $TaskDef.Actions.Create(0)
+$action.Path = "$TCmd"
+$action.Arguments = "$TArg"
+
+#Under the root folder reistration
+$Folder.RegisterTaskDefinition("$TName",$TaskDef,6,"System",$null,5)
---
+++ azureinfrastructureascode/Schedulebackup.ps1
@@ -0,0 +1,44 @@
+﻿<#
+    .SYNOPSIS
+        Template script
+    .DESCRIPTION
+        This script sets up the basic framework that I use for all my scripts.
+    .PARAMETER
+    .EXAMPLE
+    .NOTES
+        ScriptName : ScheduleBackup.ps1
+        Created By : Manimaran Chandrasekaran
+        Date Coded : 12/19/2014 07:51:20
+
+    .LINK
+        https://www.devopspractice.com/
+#>
+
+$key = "GxAgGBZuxwRUp+JF6DR7QNO/YAoIrckSgLv0jxOiZpS85"
+$StorageAccName = "azurenetworkfileshares"
+
+# Below simple lines of code is used to make the Azure file shares
+cmdkey /add:$StorageAccName.file.core.windows.net /user:$StorageAccName /pass:$key
+$AzureStorageShareName = "stagearea"
+
+$cmdnetshare = "net use x: \\$StorageAccName.file.core.windows.net\$AzureStorageShareName"
+#Invoke-Expression -Command $cmdnetshare
+$from = "C:\DemoSource"
+$to = "C:\backups\"
+$objSvc = Get-CimInstance -ComputerName $server -Query "SELECT Name, StartName, Status, State FROM Win32_Service WHERE StartName <> 'LocalSystem'" | ? { $_.StartName -notlike 'NT AUTHORITY*' -and $_.StartName -notlike 'NT SERVICE*' } | Select *
+    if ($objSvc.Name -eq 'EventStoreService')
+    {
+        Write-Host "Service is installed on the destination server"
+                
+    }
+    else
+    {
+        Write-Host "Service is not installed on the destination server"
+    }
+$timestamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
+#Create the backup directory if does not exists
+New-Item -ItemType Directory -Path $to\logs
+#Backup to the azure file shares
+Robocopy $from $to /MT:20 > $to\logs\$timestamp.log
+
+
